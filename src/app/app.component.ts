a3d0af2d239675eb9843359a4c5c97b6e62f73b6
alex.fan <alex.fan@okchem.com>
2019-08-26 17:06:52 +0800

    basic service injection via provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PeopleService } from "./services/people.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  people: Array<any>;
 
+  constructor(private peopleService: PeopleService) {
+    this.people = peopleService.getPeople();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { PeopleService } from "./services/people.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
